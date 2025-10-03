4ea766e44cca7926d5f3cdbdce8b29cc55155f40
Kalkidan Adeba <167715392+kal1621@users.noreply.github.com>
2025-10-03 13:30:28 +0400

    Update Views.py

--- messaging_app/chats/Views.py
+++ messaging_app/chats/Views.py
@@ -1,13 +1,20 @@
+
+
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Message
 from .serializers import MessageSerializer
 from .permissions import IsAuthenticatedAndParticipant
+from .filters import MessageFilter
+from .pagination import MessagePagination  # Import your custom pagination class
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticatedAndParticipant]
+    pagination_class = MessagePagination  # Use custom pagination class
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = MessageFilter  # Use the filtering class
 
     def get_queryset(self):
-        # Only return messages that belong to the authenticated user's conversations
         return self.queryset.filter(conversation__participants=self.request.user)
