e5fd9a2796eea7298641877dee5107e695ca09d8
Kalkidan Adeba <167715392+kal1621@users.noreply.github.com>
2025-10-03 13:28:52 +0400

    Update settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -7,3 +7,8 @@ REST_FRAMEWORK = {
         # Add other authentication classes if needed
     ),
 }
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,  # Set the number of messages per page
+    ...
+}
